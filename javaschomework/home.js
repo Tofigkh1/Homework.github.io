c789ce3bc821c41ab4ea159421b77c6d66da85d9
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 <khaniyevdevelop@gmail.com>
2023-10-30 16:27:07 +0400

    restore

--- javaschomework/home.js
+++ javaschomework/home.js
@@ -74,112 +74,127 @@
 // console.log(mystring2);
 
 
-// var mystring2 = 
 
 
+// function convertStringToArray(text) {
 
-
-
-// var nums = [12, 32, 54, 65];
-// var arr2 = [];
-
-// for(var elements of nums){
-
-//     elements2 = elements * 2
-//     arr2.push(elements2)
-
+//     var text2 = text.toLowerCase().split("");
+//     return text2
 // }
 
-// console.log(arr2);
 
+// var result = convertStringToArray("Ayaz");
 
-// var list = [
-//     "John",
-//     2000,
-//     32,
-//     true,
-//     "Marry",
-//     1200,
-//     24,
-//     false,
-//     "Siri",
-//     3000,
-//     26,
-//     false,
-//   ]
-
-//  for(i=0; i<list.length; i++){
-
-//     if(typeof list[i] === "string"){
-//         console.log(list[i]);
-//     }
-//  }
-
-//  var i=0
-
-//  while(i<list.length){
-
-//     if(typeof list[i] === "string"){
-
-//         console.log(list[i]);
-//     }
-//     i++
-//  }
-
-
-
-
-
-// var a = [5,6,8];
-
-// var b = a.concat
-
-// b[0]=7
-
-// console.log(a);
-
-
-
-// var a = [5,4];
-
-// var b = [45,3];
-
-// var c =a.concat(8,13);
-
-// a[0]=101;
-
-// console.log(c);
-
-
-
-// inversion method
-
-// var a = [5,4,7,6,3];
-
-// a.reverse();
-
-// console.log(a);
-
-
-
-// var arr = [5,8,14,55];
-
-// var result = arr.indexOf(56);
 // console.log(result);
 
+// function convertSanToTime(num) {
+
+//     var min = num/60;
+//     var hour = num/3600;
+//     return `hour: ${min} or min: ${hour}`
+// }
 
 
-var fullname = prompt("adinizi qeyd edin");
+// console.log(convertSanToTime(9250));
 
-var result1 = fullname.startsWith("a");
 
-var result2 = fullname.endsWith("a")
 
-if(result1 == true && result2 == true){
-    console.log("girdiniz");
+// var usarname = prompt('xais olunur adinizi qeyd edin:');
 
-}else{
-    console.log("olmadi");
+// var usarage = prompt('xais olunur yasinizi qeyd edin:');
+
+// var usarjob = prompt('xais olunur calistiginiz vezifeni qeyd edin:');
+
+// function showUserInfo(names1 = "usar", age="age1", job="job1"){
+//     var result1 = `${names1}, sizin ${age} yasiniz var siz ${job} olaraq calisirsiniz`
+
+//     if(names1==null){
+//         names1=0
+
+//     }if(age==null){
+//         age=0
+
+//     }if(job==null){
+//         job=0
+//     }
+//     return result1
+// }
+
+
+// console.log(showUserInfo(usarname,usarage,usarjob));
+
+
+
+
+
+// var nums = [2,3,4,5,6] //! [6,5,4,3,2]
+
+
+// function reverseArray(arr){
+//     var reverarray2 = [];
+//     for(var i = arr.lenght - 1; i>=0, i--) {
+
+//         reverarray2.push(arr[i]);
+
+//     }
+
+//     return reverarray2
+// }
+
+
+// var reversarray3 = reverseArray(nums) 
+// console.log(reversarray3);
+
+
+
+// var nums  = [21,43,101,2,10]
+
+
+// function maxOrMinNumber(arr,method){
+
+// for (var index = 1; index < arr.length; index++) {
+//     if(method) {
+//         if(arr[index]> arr[index+1]){
+//             console.log(arr[index]);e    
+            
+//         }
+//     }else{ 
+//         if(arr[index]< arr[index+1]){
+//             console.log(arr[index]);
+//         } 
+//     }
+//     // if(arr[index]< arr[index+1]) {
+//     //     console.log(arr[index]);
+//     //     }else 
+// }
+    
+// }
+
+// // maxOrMinNumber(nums,false) 
+// maxOrMinNumber(nums,true)
+
+// var numbers = [12, 4, 17, 10, 12, 7, 29];
+
+
+// function myfunc(numbers){
+// var sum = 0;
+
+
+// for(var i = 0; i < numbers.length; i++){
+//     sum+=numbers[i]
+// }
+// return sum
+// }
+// myfunc(numbers);
+// console.log(myfunc(numbers));
+
+// var result = numbers.reduce(function(sum,el){
+//     return sum + el;
+//  },0)
+
+
+
+for(var i = 0; i<20; i+=3){
+    console.log("index", i);
 }
 
-console.log(result1);
